9cd3d9b3835fed959dad4e1c33add1a57d38f9a5
VerneriLahteenoja <verneri.lahteenoja@gmail.com>
2024-02-29 15:58:44 +0200

    3.2 step2

--- index.js
+++ index.js
@@ -4,14 +4,22 @@ const app = express()
 
 const PORT = 3001
 
-app.get('/', (req, res) => {
-    res.send('<h1>Hello World!</h1>')
-})
 
 app.get('/api/numbers', (req, res) => {
     res.json(numbers)
 })
+console.log(numbers)
+app.get('/info', (req, res) => {
+    const date = Date()
+    res.send(
+        `<div>Phonebook has info for ${numbers.length} people</div>
+        </br>
+        <div>${date}</div>
+        `)
+})
 
+
+// RUN
 app.listen(PORT, () => {
     console.log(`Running on port ${PORT}`)
 })
