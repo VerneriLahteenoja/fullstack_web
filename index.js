ad1632d363c8cc710df5693b31898a020c442724
VerneriLahteenoja <verneri.lahteenoja@gmail.com>
2024-02-29 16:11:33 +0200

    3.3 step3

--- index.js
+++ index.js
@@ -1,21 +1,32 @@
-const numbers = require('./numbers.json')
+const persons = require('./persons.json')
 const express = require('express')
 const app = express()
 
 const PORT = 3001
 
 
-app.get('/api/numbers', (req, res) => {
-    res.json(numbers)
+app.get('/api/persons', (req, res) => {
+    res.json(persons)
 })
-console.log(numbers)
+console.log(persons)
 app.get('/info', (req, res) => {
     const date = Date()
-    res.send(
-        `<div>Phonebook has info for ${numbers.length} people</div>
+    res.send
+    (`<div>Phonebook has info for ${persons.length} people</div>
         </br>
         <div>${date}</div>
-        `)
+    `)
+})
+
+app.get('/api/persons/:id', (req, res) => {
+    const id = Number(req.params.id)
+    const person = persons.find(person => person.id === id)
+    if (person) {
+        res.json(person)
+    } else {
+        res.status(404).end()
+    }
+
 })
 
 
