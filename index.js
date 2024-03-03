777dca3ef63866b7be2a97a2274aeb027d20e536
VerneriLahteenoja <verneri.lahteenoja@gmail.com>
2024-03-03 22:06:12 +0200

    add express.static

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ const cors = require('cors')
 
 app.use(express.json())
 app.use(cors())
+app.use(express.static('dist'))
 
 morgan.token('body', function (req, res) {return req.method==='POST' ? JSON.stringify(req.body) : ''})
 app.use(morgan(function (tokens, req, res) {
