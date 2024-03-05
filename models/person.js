42df1c8494c47a0cce943f7ac59ec41a5bc543b7
VerneriLahteenoja <verneri.lahteenoja@gmail.com>
2024-03-05 23:38:42 +0200

    create models for MongoDB connections

---
+++ models/person.js
@@ -0,0 +1,56 @@
+const mongoose = require('mongoose')
+require('dotenv').config()
+mongoose.set('strictQuery', false)
+
+const url = process.env.MONGODB_URI
+
+console.log(`Connecting to url: ${url}`)
+
+mongoose.connect(url)
+  .then(result => {
+    console.log("connected to MongoDB")
+  })
+  .catch((error) => {
+    console.log(`error connecting to MongoDB: ${error.message}`)
+  })
+
+// Schema
+const personSchema = new mongoose.Schema({
+  name: String,
+  number: String,
+})
+
+personSchema.set('toJSON', {
+  transform: (document, returnedObject) => {
+    returnedObject.id = returnedObject._id.toString()
+    delete returnedObject._id
+    delete returnedObject.__v
+  }
+})
+
+const Person = mongoose.model('Person', personSchema)
+
+//   const person = new Person({
+//     name: `${name}`,
+//     number: `${number}`,
+//   })
+
+//   person.save().then(result => {
+//     console.log(`added ${name} ${number} to phonebook`)
+//     mongoose.connection.close()
+//   })
+
+// } else {
+//   // Get all objects in Person
+//   Person.find({}).then(result => {
+//     result.forEach(person => {
+//       console.log(person)
+//     })
+//     mongoose.connection.close()
+//   })
+// }
+
+module.exports = Person
+
+
+
